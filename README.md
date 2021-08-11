20ef6a6babdd8f0f45f530ae400f8d6d30355e87
Jamie Barton <jamie@notrab.dev>
2021-08-11 13:11:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fnotrab%2Fshortcuts&env=SHORTCUTS_NAME,SHORTCUTS_TWITTER,SHORTCUTS_YOUTUBE)
---
+++ generate.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const fs = require("fs");
+const path = require("path");
+const handlebars = require("handlebars");
+
+const template = fs.readFileSync(
+  path.resolve(path.join(__dirname, "template.hbs")),
+  "utf-8"
+);
+
+const data = {
+  name: process.env.SHORTCUTS_NAME,
+  twitter: process.env.SHORTCUTS_TWITTER,
+  youtube: process.env.SHORTCUTS_YOUTUBE,
+};
+
+const output = handlebars.compile(template)(data);
+
+fs.writeFile("index.html", output, (err) => {
+  if (err) return console.log(err);
+
+  console.log("Generated output");
+});
